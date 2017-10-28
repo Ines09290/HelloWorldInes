6ec10ef2fe740f9b345cf3c1e65a45b9e4382559
Ines09290 <ines_merlin@hotmail.com>
2017-10-28 20:29:24 +0200

    Entrega Hello World Inés 09290

---
+++ src/HelloWorldInes.cpp
@@ -0,0 +1,14 @@
+/*
+ * HelloWorldInes.cpp
+ *
+ *  Created on: 28 oct. 2017
+ *      Author: Inés
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
